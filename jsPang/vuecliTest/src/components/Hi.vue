d86002768b164080022a55ae85c94529c5a94ef8
oral0717 <807910824@qq.com>
2020-03-07 23:40:18 +0800

    初级路由

---
+++ jsPang/vuecliTest/src/components/Hi.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hi">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name: 'Hi',
+  data () {
+    return {
+      msg: 'hi页面'
+    }
+  }
+}
+</script>
+
+<style>
+.hi {border:1px solid red;}
+</style>
