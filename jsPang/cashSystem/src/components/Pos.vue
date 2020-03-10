edd988b06b9c8efc86e7bab321f5b4c5a418812f
oral0717 <807910824@qq.com>
2020-03-10 16:34:21 +0800

    加入element-ui

---
+++ jsPang/cashSystem/src/components/Pos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>{{msg}}</div>
+    <el-button type="success">默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Button } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(Button)
+
+export default {
+  data () {
+    return {
+      msg: 'Pos页面'
+    }
+  }
+}
+</script>
