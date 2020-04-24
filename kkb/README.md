7a9fede86440d463d6bdcce919451588a768ce68
oral0717 <807910824@qq.com>
2020-04-24 11:03:49 +0800

    初级购物车功能

--- kkb/README.md
+++ kkb/README.md
@@ -23,3 +23,11 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 目录说明
+1. public 静态资源：图片、模板
+2. src 开发代码
+      组件
+      首页
+      工具函数
+      入口：main.js
--- kkb/src/App.vue
+++ kkb/src/App.vue
@@ -1,28 +1,63 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>xx{{count}}</h1>
+    <ul>
+      <li v-for="(good, index) in goods" :key="good.id">
+        {{index + 1}}: {{good.text}} | {{good.price}}
+        <button @click="addCart(index)">加入购物车</button>
+      </li>
+    </ul>
+    <p>购物车展示</p>
+    <table border="1">
+      <thead>
+        <th>商品编码</th>
+        <th>商品名称</th>
+        <th>商品单价</th>
+        <th>数量</th>
+        <th>总价</th>
+      </thead>
+      <tbody>
+        <tr v-for="good in cart" :key="good.id">
+          <td>{{good.id}}</td>
+          <td>{{good.text}}</td>
+          <td>{{good.price}}</td>
+          <td>{{good.count}}</td>
+          <td>{{good.price*good.count}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      count: 1,
+      goods: [
+        {id: 123, text: "商品一", price: 10},
+        {id: 245, text: "商品二", price: 20},
+        {id: 3674, text: "商品三", price: 50},
+      ],
+      cart: []
+    }
+  },
+  methods: {
+    addCart(index) {
+      let item = this.goods[index]
+      let good = this.cart.find(v => v.id === item.id)
+      if (good) {
+        good.count += 1
+      } else {
+        this.cart.push({...item, count:1})
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+h1 {
+  color: red;
 }
 </style>
