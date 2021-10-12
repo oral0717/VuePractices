99f014ae030211c9e15c435732da13252be0b436
oral0717 <807910824@qq.com>
2021-10-12 17:41:26 +0800

    update

---
+++ vue3/demo1/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    publicPath: "./", //引入js、图片等文件中，会自动拼接path
+    assetsDir: "static", // 编译后的静态资源目录
+    indexPath: "index.html",
+    filenameHashing: true, // 文件名hash
+    outputDir: "dist",
+    productionSourceMap: false, // 是否生成.map
+  };
+
+
